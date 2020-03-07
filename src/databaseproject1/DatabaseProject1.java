d459ad0ca470c10c3a9969bd5c3a922001c02740
rodew <60420272+rodew374@users.noreply.github.com>
2020-03-07 15:40:27 -0800

    Added printData() method.

--- src/databaseproject1/DatabaseProject1.java
+++ src/databaseproject1/DatabaseProject1.java
@@ -6,32 +6,46 @@ import java.util.logging.Logger;
 
 public class DatabaseProject1 {
 
-    public static void main(String[] args)  {
+    private static Connection con;
+
+    public static void main(String[] args) throws SQLException {
         DatabaseProject1 pro = new DatabaseProject1();
         pro.createConnection();
+        printData("users");
+        printData("users2");
 
     }
 
-    void createConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "7@AyCXG3");
+    static void printData(String table) throws SQLException {
 
-            Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM users/* WHERE name like 'A%'*/");
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("SELECT * FROM " + table);
+        ResultSetMetaData md = rs.getMetaData();
+        if (md.getColumnCount()==1) {
             while(rs.next()) {
                 String name = rs.getString("name");
                 System.out.println(name);
 
             }
 
-            rs = stmt.executeQuery("SELECT * FROM users2");
+        } else {
             while(rs.next()) {
                 String name = rs.getString("name");
                 int age = rs.getInt("age");
                 System.out.println(name + " age = " + age);
 
             }
+
+        }
+
+        stmt.close();
+    }
+
+    void createConnection() {
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "7@AyCXG3");
 
             System.out.println("Database Connection Success");
 
